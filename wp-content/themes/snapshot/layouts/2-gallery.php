9966556b5fc9a885db6f3d6ab6c1ef954c396fbb
manniR <manni@manfredraggl.com>
2015-02-19 16:57:39 +0100

    Add snappshot theme

---
+++ wp-content/themes/snapshot/layouts/2-gallery.php
@@ -0,0 +1,24 @@
+            <div class="grid_6 alpha">	
+			<div class="post">
+				<h2><?php the_title(); ?></h2> 
+								
+						</div><!-- /post--> 						
+					
+            
+          </div><!-- /grid_6 omega -->
+            
+          <div class="grid_6 omega">	
+									
+						<div class="post">
+            
+            									
+						</div><!-- /post -->
+            
+           </div><!-- /grid_6 omega -->
+
+            <div style="clear:both;height:15px;"></div>
+            
+            <div>
+		<?php _e('Category',woothemes); ?>: <?php the_category(','); ?>
+            	<?php the_content(); ?>
+            </div>
